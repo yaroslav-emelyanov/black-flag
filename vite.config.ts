5acef45a344e18408a65e100fe48e0be3fbf6ff6
Yaroslav Emelyanov <yaroslav-emelyanov@users.noreply.github.com>
2023-12-24 18:16:12 +0300

    init project

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+// vite.config.ts
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+// https://vitejs.dev/guide/build.html#library-mode
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "./src/index.ts"),
+      formats: ["es"],
+    },
+  },
+  plugins: [
+    dts({
+      exclude: ["**/*.test.ts"],
+    }),
+  ],
+});
